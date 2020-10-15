6c37014620d7ebbd79748ef0b5cc4228255af8e3
ramesh <rpaudel58@gmail.com>
2020-10-15 10:44:46 +0545

    Added questions to user and info printing

---
+++ Lab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name : ");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("Enter your age : ");
+            string rawAge = Console.ReadLine();
+            int age = Convert.ToInt32(rawAge);
+
+            Console.WriteLine("Hello {0}, you are {1} years old", name, age);
+
+        }
+    }
+}
